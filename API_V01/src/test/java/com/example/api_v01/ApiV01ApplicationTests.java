405452ec659a2235d2f0a6097ea8a00790204e27
RobMaster28 <180604094+RobMaster28@users.noreply.github.com>
2025-04-14 20:32:44 -0500

    Inicializacion del backend

---
+++ API_V01/src/test/java/com/example/api_v01/ApiV01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_v01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiV01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
