ecb14ba2ebfa60e6f3106538f7fbc36f837d13be
Sergio Antonio <111243440+sergioantonioar@users.noreply.github.com>
2025-04-29 17:48:44 -0500

    Quitando metodos para pruebas y modificando properties para conexion

--- API_V01/src/main/java/com/example/api_v01/repository/ProductRepository.java
+++ API_V01/src/main/java/com/example/api_v01/repository/ProductRepository.java
@@ -9,12 +9,6 @@ import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
-    List<Product> findByName_product(String name_product);
-    List<Product> findByPrice(Double price);
-    List<Product> findByCategory(String category);
-    List<Product> findByAdditional_observation(String additional_observation);
-    List<Product> findByStock(String stock);
-    List<Product> findByAdmin(String admin);
-    List<Product> findById_Product(UUID id_product);
+    
 }
 
