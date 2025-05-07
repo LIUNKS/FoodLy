9f9f94839aee514e912b40087ce64a76adbbda95
Sergio Antonio <111243440+sergioantonioar@users.noreply.github.com>
2025-05-06 21:09:08 -0500

    Agregando documentacion de controllers

--- API_V01/src/main/java/com/example/api_v01/controller/ProductController.java
+++ API_V01/src/main/java/com/example/api_v01/controller/ProductController.java
@@ -7,6 +7,12 @@ import java.util.UUID;
 import com.example.api_v01.dto.SuccessMessage;
 import com.example.api_v01.handler.BadRequestException;
 import com.example.api_v01.handler.NotFoundException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.annotation.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,6 +33,23 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @Operation(
+            summary = "Obtiene todos los productos",
+            description = "Este endpoint devuelve una lista de productos disponibles en el sistema."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operación exitosa",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Resource.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se encontraron productos",
+                    content = @Content
+            )
+    })
+
     // Implementacion de los metodos aqui
     @GetMapping("/list")
     public ResponseEntity<?> getAllProducts() {
@@ -38,6 +61,22 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(successMessage);
     }
 
+    @Operation(
+            summary = "Obtiene un producto por ID",
+            description = "Este endpoint devuelve un producto disponible en el sistema indicando su UUID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operación exitosa",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Resource.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se encontro producto",
+                    content = @Content
+            )
+    })
     @GetMapping("/{id}")
     public ResponseEntity<?> getProduct(@PathVariable UUID id) throws NotFoundException {
         SuccessMessage<Product> successMessage = SuccessMessage.<Product>builder()
@@ -48,12 +87,44 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(successMessage);
     }
 
+    @Operation(
+            summary = "Eliminar un producto por ID",
+            description = "Este endpoint elimina un producto disponible en el sistema indicando su UUID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operación exitosa",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Resource.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se encontro producto",
+                    content = @Content
+            )
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteProduct(@PathVariable UUID id) throws NotFoundException {
         productService.deleteProduct(id); //llamada
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build(); // respuesta 204 exito
     }
 
+    @Operation(
+            summary = "Actualizar un producto por ID",
+            description = "Este endpoint devuelve un producto disponible en el sistema indicando su UUID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operación exitosa",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Resource.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se encontro producto",
+                    content = @Content
+            )
+    })
     @PatchMapping("/{id}")
     public ResponseEntity<?> updateProduct(@PathVariable UUID id, @RequestBody ProductDTO productDTO) throws NotFoundException {
         Product updateProduct = productService.updateProduct(id, productDTO);
@@ -65,6 +136,22 @@ public class ProductController {
         return ResponseEntity.ok(successMessage);
     }
 
+    @Operation(
+            summary = "Crear un producto",
+            description = "Este endpoint crea un producto en el sistema."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operación exitosa",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Resource.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Error",
+                    content = @Content
+            )
+    })
     @PostMapping("/")
     public ResponseEntity<?> CreateProduct(@RequestBody ProductDTO productDTO) {
         Product product = productService.saveProduct(productDTO);
@@ -84,16 +171,37 @@ public class ProductController {
         return ResponseEntity.created(location).body(successMessage);
     }
 
+    @Operation(
+            summary = "Obtener producto",
+            description = "Lanza una excepción en caso de que no se proporcione el ID del producto."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "ID del producto faltante. Se necesita el ID del producto para buscarlo.")
+    })
     @GetMapping("/")
     public ResponseEntity<?> getProductId() throws BadRequestException {
         throw new BadRequestException("Se necesita el ID del producto para buscarlo");
     }
 
+    @Operation(
+            summary = "Actualizar producto",
+            description = "Lanza una excepción en caso de que no se proporcione el ID del producto para la actualización."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "ID del producto faltante. Se necesita el ID del producto para actualizarlo.")
+    })
     @PutMapping("/")
     public ResponseEntity<?> updateProductId() throws BadRequestException {
         throw new BadRequestException("Se necesita el ID del producto para actualizarlo");
     }
 
+    @Operation(
+            summary = "Eliminar producto",
+            description = "Lanza una excepción en caso de que no se proporcione el ID del producto para eliminarlo."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "ID del producto faltante. Se necesita el ID del producto para eliminarlo.")
+    })
     @DeleteMapping("/")
     public ResponseEntity<?> deleteProductId() throws BadRequestException {
         throw new BadRequestException("Se necesita el ID del producto para eliminarlo");
--- API_V01/src/main/java/com/example/api_v01/controller/StockController.java
+++ API_V01/src/main/java/com/example/api_v01/controller/StockController.java
@@ -8,6 +8,12 @@ import com.example.api_v01.handler.NotFoundException;
 import com.example.api_v01.model.ProductStock;
 import com.example.api_v01.service.product_stock_service.ProductStockService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.annotation.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,6 +33,22 @@ public class StockController {
         this.productStockService = productStockService;
     }
 
+    @Operation(
+            summary = "DESCONTINUADO",
+            description = "No usar"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operación exitosa",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Resource.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se encontro producto",
+                    content = @Content
+            )
+    })
     //Posiblemente se descontinue
     @PostMapping("/{id_product}")
     public ResponseEntity<?> CreateStock(@PathVariable UUID id_product,@RequestBody ProductStockDTO productStockDTO) throws NotFoundException {
@@ -45,6 +67,22 @@ public class StockController {
         return ResponseEntity.created(location).body(successMessage);
     }
 
+    @Operation(
+            summary = "Obtiene todos los stocks",
+            description = "Este endpoint lista stock de todos los productos disponibles en el sistema."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operación exitosa",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Resource.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se encontraron stocks",
+                    content = @Content
+            )
+    })
     @GetMapping("/list")
     public ResponseEntity<?> getAllStocks() {
         SuccessMessage<List<ProductStock>> successMessage = SuccessMessage.<List<ProductStock>>builder()
@@ -55,6 +93,22 @@ public class StockController {
         return ResponseEntity.ok(successMessage);
     }
 
+    @Operation(
+            summary = "Obtiene stock por ID",
+            description = "Este endpoint obtiene stock de un producto disponible en el sistema indicando UUID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operación exitosa",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Resource.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se encontro stock",
+                    content = @Content
+            )
+    })
     @GetMapping("/{id_stock}")
     public ResponseEntity<?> getStock(@PathVariable UUID id_stock) throws NotFoundException {
         ProductStock productStock = productStockService.getProductStockById(id_stock);
@@ -66,13 +120,44 @@ public class StockController {
         return ResponseEntity.ok(successMessage);
     }
 
-
+    @Operation(
+            summary = "Elimina stock por ID",
+            description = "Este endpoint elimina stock de un producto disponible en el sistema indicando UUID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operación exitosa",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Resource.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se encontro stock",
+                    content = @Content
+            )
+    })
     @DeleteMapping("/{id_stock}")
     public ResponseEntity<?> deleteStock(@PathVariable UUID id_stock) throws NotFoundException {
         productStockService.deleteProductStock(id_stock);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+            summary = "Limpia stock por ID",
+            description = "Este endpoint limpia stock del producto disponible en el sistema indicando UUID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operación exitosa",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Resource.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se encontro stock",
+                    content = @Content
+            )
+    })
     @PostMapping("/clean/{id_stock}")
     public ResponseEntity<?> cleanStock(@PathVariable UUID id_stock) throws NotFoundException {
         ProductStock stock = productStockService.cleanStockById(id_stock);
@@ -84,6 +169,22 @@ public class StockController {
         return ResponseEntity.ok().body(successMessage);
     }
 
+    @Operation(
+            summary = "Descuenta stock por ID",
+            description = "Este endpoint descuenta stock del producto disponible en el sistema indicando UUID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operación exitosa",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Resource.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se encontro stock",
+                    content = @Content
+            )
+    })
     @PostMapping("/discount/{id_stock}")
     public ResponseEntity<?> discountStock(@PathVariable UUID id_stock,@RequestParam Integer count) throws NotFoundException,BadRequestException {
         ProductStock stock = productStockService.discountStockById(id_stock,count);
@@ -95,6 +196,22 @@ public class StockController {
         return ResponseEntity.ok().body(successMessage);
     }
 
+    @Operation(
+            summary = "Incrementar stock de un producto por ID",
+            description = "Este endpoint incrementa stock del producto disponible en el sistema indicando UUID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operación exitosa",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Resource.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se encontro stock",
+                    content = @Content
+            )
+    })
     @PostMapping("/increase/{id_stock}")
     public ResponseEntity<?> increaseStock(@PathVariable UUID id_stock,@RequestParam Integer count) throws NotFoundException{
         ProductStock stock = productStockService.increaseStockById(id_stock,count);
@@ -113,16 +230,37 @@ public class StockController {
     }
 
 
+    @Operation(
+            summary = "Obtener stock por ID",
+            description = "Este endpoint lanza una excepción si no se proporciona el ID necesario para buscar el stock."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "No se proporcionó el ID del stock. Es obligatorio para la búsqueda.")
+    })
     @GetMapping("/")
     public ResponseEntity<?> getStockId() throws BadRequestException {
         throw new BadRequestException("Se necesita el ID del stock para buscarlo");
     }
 
+    @Operation(
+            summary = "Actualizar stock",
+            description = "Este endpoint lanza una excepción si no se proporciona el ID necesario para actualizar el stock."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "No se proporcionó el ID del stock. Es obligatorio para actualizarlo.")
+    })
     @PutMapping("/")
     public ResponseEntity<?> updateStockId() throws BadRequestException {
         throw new BadRequestException("Se necesita el ID del stock para actualizarlo");
     }
 
+    @Operation(
+            summary = "Eliminar stock",
+            description = "Este endpoint lanza una excepción si no se proporciona el ID necesario para eliminar el stock."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "No se proporcionó el ID del stock. Es obligatorio para eliminarlo.")
+    })
     @DeleteMapping("/")
     public ResponseEntity<?> deleteStockId() throws BadRequestException {
         throw new BadRequestException("Se necesita el ID del stock para eliminarlo");
