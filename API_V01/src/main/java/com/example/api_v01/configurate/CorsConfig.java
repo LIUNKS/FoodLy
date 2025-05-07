d79d74c7122d0ee933489bd145ebc6652c1ae52c
RobMaster28 <180604094+RobMaster28@users.noreply.github.com>
2025-05-06 19:27:53 -0500

    desarrollo de los cors

---
+++ API_V01/src/main/java/com/example/api_v01/configurate/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.example.api_v01.configurate;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000") // poner "http://localhost:3000" del frontend
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
