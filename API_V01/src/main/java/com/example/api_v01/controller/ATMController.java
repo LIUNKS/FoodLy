1ab473d0b19dae286f62e4f1532923384e5bd38c
Sergio Antonio <111243440+sergioantonioar@users.noreply.github.com>
2025-05-07 17:38:22 -0500

    agregando documentacion para controladores Box y ATM

--- API_V01/src/main/java/com/example/api_v01/controller/ATMController.java
+++ API_V01/src/main/java/com/example/api_v01/controller/ATMController.java
@@ -5,6 +5,12 @@ import com.example.api_v01.dto.RegisterATMDTO;
 import com.example.api_v01.handler.NotFoundException;
 import com.example.api_v01.model.ATM;
 import com.example.api_v01.service.atm_service.ATMService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.annotation.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +26,22 @@ public class ATMController {
         this.atmservice = atmservice;
     }
 
+    @Operation(
+            summary = "Crea un nuevo ATM",
+            description = "Este endpoint permite crear un nuevo ATM asociado a un administrador específico mediante el ID del administrador."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "ATM creado exitosamente",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ATM.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Administrador no encontrado",
+                    content = @Content
+            )
+    })
     @PostMapping("/{adminId}")
     public ResponseEntity<ATM> saveATM(@PathVariable("adminId") UUID adminId, @RequestBody AtmDTO atmDTO) {
         try {
@@ -29,6 +51,23 @@ public class ATMController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    @Operation(
+            summary = "Asigna un usuario a un ATM",
+            description = "Asigna un usuario a un ATM específico mediante el ID del ATM."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Usuario asignado al ATM exitosamente",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ATM.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "ATM no encontrado",
+                    content = @Content
+            )
+    })
 
     @PutMapping("/{atmId}/assign-user")
     public ResponseEntity<ATM> assignUserATM(@PathVariable("atmId") UUID atmId, @RequestBody RegisterATMDTO registerATMDTO) {
@@ -40,6 +79,23 @@ public class ATMController {
         }
     }
 
+    @Operation(
+            summary = "Actualiza un ATM",
+            description = "Permite actualizar la información de un ATM específico mediante su ID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "ATM actualizado exitosamente",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ATM.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "ATM no encontrado",
+                    content = @Content
+            )
+    })
+
     @PutMapping("/{atmId}")
     public ResponseEntity<ATM> updateATM(@PathVariable("atmId") UUID atmId, @RequestBody AtmDTO atmDTO) {
         try {
@@ -49,6 +105,22 @@ public class ATMController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    @Operation(
+            summary = "Elimina un ATM",
+            description = "Este endpoint elimina un ATM específico según su ID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "ATM eliminado exitosamente"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "ATM no encontrado",
+                    content = @Content
+            )
+    })
 
     @DeleteMapping("/{atmId}")
     public ResponseEntity<Void> deleteATM(@PathVariable("atmId") UUID atmId) {
@@ -60,6 +132,22 @@ public class ATMController {
         }
     }
 
+    @Operation(
+            summary = "Obtiene un ATM por ID",
+            description = "Devuelve la información de un ATM específico según el ID proporcionado."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operación exitosa",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ATM.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "ATM no encontrado",
+                    content = @Content
+            )
+    })
     @GetMapping("/{atmId}")
     public ResponseEntity<ATM> getAtmById(@PathVariable("atmId") UUID atmId) {
         try {
@@ -70,6 +158,17 @@ public class ATMController {
         }
     }
 
+    @Operation(
+            summary = "Obtiene la lista de todos los ATMs",
+            description = "Este endpoint devuelve todos los ATMs disponibles en el sistema."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operación exitosa",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ATM.class))
+            )
+    })
     @GetMapping
     public ResponseEntity<List<ATM>> getAllATMs() {
         List<ATM> atmList = atmservice.getAllATMs();
--- API_V01/src/main/java/com/example/api_v01/controller/BoxController.java
+++ API_V01/src/main/java/com/example/api_v01/controller/BoxController.java
@@ -4,6 +4,11 @@ import com.example.api_v01.dto.BoxDTO;
 import com.example.api_v01.handler.NotFoundException;
 import com.example.api_v01.model.Box;
 import com.example.api_v01.service.box_service.BoxService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +25,22 @@ public class BoxController {
         this.boxservice = boxservice;
     }
 
+    @Operation(
+            summary = "Crea una nueva caja (Box)",
+            description = "Este endpoint permite crear una nueva caja asociada a un administrador específico mediante su ID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Caja creada exitosamente",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Box.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Administrador no encontrado",
+                    content = @Content
+            )
+    })
     @PostMapping("/{adminId}")
     public ResponseEntity<Box> saveBox(@PathVariable("adminId") UUID adminId, @RequestBody BoxDTO boxDTO){
         try {
@@ -30,6 +51,22 @@ public class BoxController {
         }
     }
 
+    @Operation(
+            summary = "Cambia el estado de una caja",
+            description = "Este endpoint alterna el estado de una caja (abierta o cerrada) basado en su estado actual, identificada por su ID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Estado de la caja actualizado exitosamente",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Box.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Caja no encontrada",
+                    content = @Content
+            )
+    })
     @PutMapping("/{boxId}/toggle-status")
     public ResponseEntity<Box> toggleBoxStatus(@PathVariable("boxId") UUID boxId) {
         try {
@@ -40,6 +77,22 @@ public class BoxController {
         }
     }
 
+    @Operation(
+            summary = "Asigna un ATM a una caja",
+            description = "Este endpoint permite asignar un cajero automático (ATM) a una caja específica mediante sus IDs respectivos."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "ATM asignado a la caja exitosamente",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Box.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Caja o ATM no encontrados",
+                    content = @Content
+            )
+    })
     @PutMapping("/{boxId}/assign-atm/{atmId}")
     public ResponseEntity<Box> assignAtmToBox(@PathVariable("boxId") UUID boxId, @PathVariable("atmId") UUID atmId) {
         try {
@@ -50,6 +103,22 @@ public class BoxController {
         }
     }
 
+    @Operation(
+            summary = "Obtiene información de una caja",
+            description = "Este endpoint devuelve los detalles de una caja específica, identificada por su ID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operación exitosa",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Box.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Caja no encontrada",
+                    content = @Content
+            )
+    })
     @GetMapping("/{boxId}")
     public ResponseEntity<Box> getBoxInfo(@PathVariable("boxId") UUID boxId) {
         try {
@@ -60,12 +129,39 @@ public class BoxController {
         }
     }
 
+    @Operation(
+            summary = "Obtiene todas las cajas",
+            description = "Este endpoint devuelve una lista de todas las cajas disponibles en el sistema."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operación exitosa",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Box.class))
+            )
+    })
     @GetMapping
     public ResponseEntity<List<Box>> getBoxes() {
         List<Box> boxes = boxservice.getBoxes();
         return ResponseEntity.ok(boxes);
     }
 
+    @Operation(
+            summary = "Obtiene cajas asociadas a un ATM",
+            description = "Devuelve una lista de cajas relacionadas con un ATM específico identificado por su ID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operación exitosa",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Box.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "ATM no encontrado",
+                    content = @Content
+            )
+    })
     @GetMapping("/by-atm/{atmId}")
     public ResponseEntity<List<Box>> getBoxesByAtm(@PathVariable("atmId") UUID atmId) {
         try {
