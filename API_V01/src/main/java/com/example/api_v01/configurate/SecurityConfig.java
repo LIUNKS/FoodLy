b30c8f93d7d40009e886f521ef66b41953260665
roberto <180604094+RobMaster28@users.noreply.github.com>
2025-05-22 20:27:20 -0500

    Update SecurityConfig

--- API_V01/src/main/java/com/example/api_v01/configurate/SecurityConfig.java
+++ API_V01/src/main/java/com/example/api_v01/configurate/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST","PATCH", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
