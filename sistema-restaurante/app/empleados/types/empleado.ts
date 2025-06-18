892421245f8a4494fef162541adfc44f487be6a2
Jota <33913578+yourch22@users.noreply.github.com>
2025-06-18 18:28:04 -0500

    Agregar interface types empleado

---
+++ sistema-restaurante/app/empleados/types/empleado.ts
@@ -0,0 +1,8 @@
+export interface Empleado {
+  id_atm: string; // Identificador único del empleado
+    name_atm: string; // Nombre completo del empleado
+    alias: string; // Alias o nombre de usuario del empleado
+    email: string; // Correo electrónico del empleado
+    phone: string; // Número de teléfono del empleado
+    dni: string// Documento Nacional de Identidad del empleado
+}
